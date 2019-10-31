e8abf45c0fbe96d8f8ae0636f691add3265fa15e
Jonathan Aquino <jonathanaquino@Jonathans-MacBook-Pro.local>
2019-10-31 09:53:26 -0400

    coming back after work

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # eloquentjavascript
+
+**This is not a showcase, 
+using github to keep my current code 
+on multiple computers that I have access to**
+
+Link to excersice and project Sandbox
+
+https://eloquentjavascript.net/code/
---
+++ excersises/a_vector_type.js
@@ -0,0 +1,19 @@
+
+class Vec {
+
+}
+
+
+
+
+
+
+
+//-=-=-=-=-= Test Cases -=-=-=-=-=//
+
+// console.log(new Vec(1, 2).plus(new Vec(2, 3)));
+// // → Vec{x: 3, y: 5}
+// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+// // → Vec{x: -1, y: -1}
+// console.log(new Vec(3, 4).length);
+// // → 5

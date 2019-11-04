4b881c9720a807fec272257242be00a3d8eec622
Jonathan Aquino <jonathanaquino@Jonathans-MacBook-Pro.local>
2019-11-04 09:18:44 -0500

    minimum excersise complete

---
+++ excersises/functions.js
@@ -0,0 +1,10 @@
+//-=-=-=-=-= Exercise 3.1 - Minimum -=-=-=-=-=//
+//replicating the Math.min function
+
+const min = (a, b) => a < b ? a : b;
+
+//-=-=-=-=-= Test Cases 3.1 - Minimum -=-=-=-=-=//
+
+console.log(min(4,5));
+console.log(min(15,9));
+console.log(min(4002,5002));
